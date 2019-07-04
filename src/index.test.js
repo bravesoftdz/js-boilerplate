618f739d1faf99ed7f3b3b1497841f4116839599
Sebastian Reverso <sebastianreverso@gmail.com>
2019-07-04 20:25:27 -0300

    Fixing the execution of test that calls JSDOM

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
+const {JSDOM} = jsdom;
 import fs from 'fs';
 
 describe('Our first test', () => {
@@ -11,11 +12,16 @@ describe('Our first test', () => {
 describe('index.html', () => {
   it('should say hello', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
-    jsdom.env(index, function (err, window) {
+    const dom = new JSDOM(index);
+    const h1 = dom.window.document.getElementsByTagName('h1')[0];
+    expect(h1.innerHTML).to.equal("Hello World!");
+    done();
+    window.close();
+    /*jsdom.env(index, function (err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
       expect(h1.innerHTML).to.equal("Hello World!");
       done();
       window.close();
-    });
+    });*/
   });
 });
