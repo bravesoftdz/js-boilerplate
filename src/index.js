502817812c7f9838559b5e6af87661f88fddfb8e
Sebastian Reverso <sebastianreverso@gmail.com>
2019-07-10 15:45:44 -0300

    Adding fetch api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+import {getUsers} from "./api/userApi";
 import './index.css'
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-window.alert(`I would pay ${courseValue} for this awesome course!`);
+console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+
+// Populate table of Users via API call
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody +=
+      `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
